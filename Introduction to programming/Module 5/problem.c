7afc9b1c3d2734274c192346e3d5a211821db22c
naimuddin94 <developernaimuddin@gmail.com>
2025-04-14 11:36:36 +0600

    Prints even numbers up to a given limit.

---
+++ Introduction to programming/Module 5/problem.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main(){
+    int n;
+
+    scanf("%d", &n);
+
+    if(n<2){
+        printf("%d\n", -1);
+    }
+
+    for(int i=2; i<=n; i+=2){
+        printf("%d\n", i);
+    }
+    
+    return 0;
+}
