21b55e522117dded0b1208e9dffc0eb43470a179
naimuddin94 <developernaimuddin@gmail.com>
2025-05-04 17:16:47 +0600

    solve all practice problem

---
+++ Introduction to programming/Module 13/pra_pro_2.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+int main(){
+    int n;
+    scanf("%d", &n);
+
+    for (int i = 1; i <= n; i++)
+    {
+        int num = 1;
+
+        for (int k = 1; k <= n-i; k++)
+        {
+            printf(" ");
+        }
+        
+        for (int j = 1; j <= i; j++)
+        {
+            printf("%d ", num);
+            num++;
+        }
+        printf("\n");
+    }
+    
+    return 0;
+}
